256d32dfbf8656a90f8acd7b1f22f095b0bf7ad7
Allyson Araujo <allysoneugenio@gmail.com>
2023-01-19 23:14:24 -0300

    Aula04_OO

---
+++ src/one/digitalinnovation/oo/aula04/exercicio4_1/OperacaoMatematica.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.oo.aula04.exercicio4_1;
+
+interface OperacaoMatematica {
+
+    void soma(double operando1, double operando2);
+
+    void subtracao(double operando1, double operando2);
+
+    void multiplicacao(double operando1, double operando2);
+
+    void divisao(double operando1, double operando2);
+    
+}
